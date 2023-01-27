fe1e4915d4458eeb13aa131fe038d56993128921
Lehlohonolo Mofula <54318202+LehlohonoloMofula@users.noreply.github.com>
2023-01-27 15:19:53 +0200

    Add project files.

---
+++ YardStompers/Data/YardStompersDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace YardStompers.Data
+{
+    public class YardStompersDbContext : DbContext
+    {
+        public YardStompersDbContext(DbContextOptions<YardStompersDbContext> options) 
+            :base(options) {  }
+    }
+    prop
+}
---
+++ YardStompers/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YardStompers.Models
+{
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
