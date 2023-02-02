53ff460b5a9696ca0348f1bc8d484473c0dd4cb2
Lehlohonolo Mofula <lehlohonoloharison@gmail.com>
2023-02-03 00:52:32 +0200

    56

--- YardStompers/Models/ApplicationType.cs
+++ YardStompers/Models/ApplicationType.cs
@@ -8,7 +8,6 @@ namespace YardStompers.Models
         public Guid Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public Guid DisplayOrder { get; set; }
         public DateTime CreatedAt { get; set}
         public DateTime? ModifiedAt { get; set}
     }
