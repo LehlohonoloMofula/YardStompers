6e6ee2843ca709713df6f4acaedecdf62c8af6c8
Lehlohonolo Mofula <lehlohonoloharison@gmail.com>
2023-02-07 17:02:30 +0200

    applicationType controler

--- YardStompers/Controllers/ApplicationTypesController.cs
+++ YardStompers/Controllers/ApplicationTypesController.cs
@@ -4,24 +4,65 @@ namespace YardStompers.Controllers
 {
     public class ApplicationTypesController : Controller
     {
+       private readonly YardStompersDbContext _DbContext;
+
+        public ApplicationTypesController(YardStompersDbContext DbContext)
+        {
+            _DbContext = DbContext;
+        }
+    
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<ApplicationType> applicationType = _DbContext.ApplicationTypes;
+            return View(applicationType);
         }
-
         public IActionResult Create()
         {
             return View();
         }
-
-        public IActionResult Edit()
+ 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType applicationType)
         {
-            return View();
+            if(ModelState.IsValid)
+            {
+                _DbContext.ApplicationTypes.Add(applicationType);
+                applicationType.CreatedAt = DateTime.Now;
+                _DbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(applicationType);
+        } 
+
+        public IActionResult Edit(Guid Id)
+        {
+            IEnumerable<ApplicationType> applicationType = _DbContext.ApplicationTypes;
+            return View(applicationType );
         }
-
-        public IActionResult Delete()
+        [HttpPut]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType applicationType)
         {
-            return View();
+            if(ModelState.IsValid)
+            {
+                _DbContext.ApplicationTypes.Update(applicationType);
+                applicationType.ModifiedAt = DateTime.Now;
+                _DbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(applicationType);
+        } 
+
+        public IActionResult Delete(Guid Id)
+        {
+           
+            var applicationType = _DbContext.ApplicationTypes.Find(Id);
+            if(applicationType == null)
+            {
+                return NotFound();
+            }
+            return View(applicationType);
         }
     }
 }
